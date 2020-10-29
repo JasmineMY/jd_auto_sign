49148c3045e34e6afbaaa1693cd60c41f979d6d5
ExplorerLog <850345193@qq.com>
2020-10-29 10:43:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const exec = require('child_process').execSync
 const fs = require('fs')
 const rp = require('request-promise')
 const download = require('download')
+const core = require('@actions/core');
 
 // 公共变量
 const KEY = process.env.JD_COOKIE
@@ -16,26 +17,34 @@ async function downFile () {
 }
 
 async function changeFiele () {
-   let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
-   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`)
-   await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
+    let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
+    content = content.replace(/var Key = ''/, `var Key = '${KEY}'`)
+    await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
 }
 
 async function start() {
-  if (!KEY) {
-    console.log('请填写 key 后在继续')
-    return
-  }
-  // 下载最新代码
-  await downFile();
-  console.log('下载代码完毕')
-  // 替换变量
-  await changeFiele();
-  console.log('替换变量完毕')
-  // 执行
-  await exec("node JD_DailyBonus.js >> result.txt");
-  console.log(fs.readFileSync('result.txt','utf-8'));
-  console.log('执行完毕')
+    
+    if (!KEY) {
+        console.log('请填写 key 后在继续')
+        return
+    }
+    
+    await downFile();
+    console.log('下载代码完毕');
+    
+    await changeFiele();
+    console.log('替换变量完毕');
+
+    await exec("node JD_DailyBonus.js >> result.txt");
+    
+    let result = fs.readFileSync('result.txt','utf-8');
+    console.log(result);
+    
+    if (/Cookie失效/.test(result.toString())) {
+        core.setFailed(`Action failed with error !!! Cookie 失效 !!!`);
+    } else {
+        console.log('执行完毕');
+    }
 }
 
 start()
